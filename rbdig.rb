8784b26e41dc0ea5abd2a008a3341e18c324215f
Darius Pirvulescu <organicdarius@gmail.com>
2025-02-25 14:27:34 -0300

    Add lookup method

--- rbdig.rb
+++ rbdig.rb
@@ -3,7 +3,10 @@ require_relative './reader'
 
 INITIAL_DNS_SERVER = '8.8.8.8'
 DOMAIN = ARGV[0]
+# INITIAL_DNS_SERVER = '8.8.8.8'
+ROOT_DNS_SERVER = '199.7.83.42' # l.root-servers.net
 DNS_PORT = 53
+MAX_LOOKUPS = 15
 
 class DNSQuery
   def initialize(query_id, q_type = "\x00\x01")
@@ -146,8 +149,40 @@ def connect(message, server = INITIAL_DNS_SERVER, port = DNS_PORT)
   response
 end
 
-msg = DNSQuery.new("\xBE\xEF").query_message(DOMAIN)
-socket_response = connect(msg)
-dns_response = DNSResponse.new(socket_response).parse
+# LOOKUP
+def lookup(domain)
+  nameserver = ROOT_DNS_SERVER
 
-p dns_response
+  for i in 1..MAX_LOOKUPS do
+    puts "Querying #{nameserver} for #{domain}"
+    query_id = [i].pack('n')
+    msg = DNSQuery.new(query_id).query_message(domain)
+    socket_response = connect(msg, nameserver)
+    if socket_response[0..1] != query_id
+      p socket_response
+      raise "Invalid response"
+    end  
+
+    dns_response = DNSResponse.new(socket_response).parse
+    if dns_response.answers.any?
+      return dns_response.answers[0][:rr_data]
+    end
+
+    if dns_response.additional.any?
+      nameserver = dns_response.additional[0][:rr_data]
+      next
+    end
+
+    if dns_response.authorities.any?
+      ns_name = dns_response.authorities[0][:rr_data]
+      nameserver = lookup(ns_name)
+      next
+    end
+  end
+
+  raise "Max lookups reached"
+end
+
+answer = lookup(DOMAIN)
+puts "\n###\n\n✅ Found answer: "
+puts answer
