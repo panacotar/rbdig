b36bfdac93b14b15687b3b19c52f900963797f65
Darius Pirvulescu <organicdarius@gmail.com>
2025-03-04 22:44:57 -0300

    Add Display class

--- dig.rb
+++ dig.rb
@@ -7,3 +7,5 @@ DOMAIN = ARGV[0]
 
 # answer = RbDig::NS.new.lookup(DOMAIN)
 p answer
+answer = RbDig::NS.new.lookup(DOMAIN)
+RbDig::Display.new(answer).pretty_print
---
+++ lib/rbdig/display.rb
@@ -0,0 +1,48 @@
+class RbDig::Display
+  def initialize(response_object)
+    @response = response_object
+  end
+
+  def pretty_print
+    puts "HEADER: opcode: QUERY, id: #{@response.header[:query_id]}"
+    # puts "flags: #{}"
+    puts "ANSWER: #{@response.answers.size}, AUTHORITY: #{@response.authorities.size}, ADDITIONAL: #{@response.additional.size}"
+    puts "\n\n"
+
+    puts 'QUESTION SECTION:'
+    puts "#{@response.body[:question]}\t\t#{q_type(@response.body[:q_type])}"
+    puts "\n"
+
+    print_answer
+    print_authority
+  end
+
+  def print_answer
+    return unless @response.answers.any?
+
+    puts 'ANSWER SECTION:'
+    @response.answers.each { |a| print_resource(a) }
+  end
+
+  def print_authority
+    return unless @response.authorities.any?
+
+    puts 'AUTHORITY SECTION:'
+    @response.authorities.each { |a| print_resource(a) }
+  end
+
+  def print_additional
+    return unless @response.additional.any?
+
+    puts 'ADDITIONAL SECTION:'
+    @response.additional.each { |a| print_resource(a) }
+  end
+
+  def print_resource(r)
+    puts "#{r[:rr_name]}\t\t#{r[:ttl]}\tIN\t#{q_type(r[:rr_type])}\t#{r[:rr_data]}"
+  end
+
+  def q_type(type)
+    RbDig::QUERY_TYPES[type]
+  end
+end
